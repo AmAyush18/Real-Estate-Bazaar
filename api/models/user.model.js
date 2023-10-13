e3041b0ee987ba9c62c417950b8e9026e8a2df6a
Ayush Dwivedi <amayush0818@gmail.com>
2023-10-14 01:42:03 +0530

    Profile page implemented

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
     },
     avatar: {
         type: String,
-        default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Fprofile-view&psig=AOvVaw1Evs6sHOQUcgBW3y1xGcFq&ust=1697264464307000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCODGkv6w8oEDFQAAAAAdAAAAABAE"
+        default: "https://www.kasandbox.org/programming-images/avatars/spunky-sam.png"
     }
 }, { timestamps: true });
 
