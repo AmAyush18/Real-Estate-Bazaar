5bf3d605fabfefb85ed925902a8ff309afaad848
Ayush Dwivedi <amayush0818@gmail.com>
2023-10-13 12:32:46 +0530

    Google Auth added

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -14,6 +14,10 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
+    },
+    avatar: {
+        type: String,
+        default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Fprofile-view&psig=AOvVaw1Evs6sHOQUcgBW3y1xGcFq&ust=1697264464307000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCODGkv6w8oEDFQAAAAAdAAAAABAE"
     }
 }, { timestamps: true });
 
