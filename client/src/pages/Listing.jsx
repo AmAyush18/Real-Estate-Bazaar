f113e4ba45f894503465c334ff998446072bfcf8
Ayush Dwivedi <amayush0818@gmail.com>
2023-10-21 16:27:22 +0530

    Implemented Listing Page UI

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,23 +1,37 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore from 'swiper';
 import { Navigation } from 'swiper/modules';
 import 'swiper/css/bundle';
+import {
+    FaBath,
+    FaBed,
+    FaChair,
+    FaMapMarkedAlt,
+    FaMapMarkerAlt,
+    FaParking,
+    FaShare,
+  } from 'react-icons/fa';
+import { Contact } from '../components/index'
 
 const Listing = () => {
     SwiperCore.use([Navigation]);
     const [listing, setListing] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
+    const [copied, setCopied] = useState(false);
+    const [contact, setContact] = useState(false);
     const params = useParams();
+    const { currentUser } = useSelector((state) => state.user);
 
     useEffect(() => {
-        const fetchListing = async (listingId) => {
+        const fetchListing = async () => {
             setLoading(true);
             setError(false);
             try {
-                const res = await fetch(`/api/listing/get/${listingId}`);
+                const res = await fetch(`/api/listing/get/${params.listingId}`);
                 const data = await res.json();
                 if(data.success === false) {
                     setError(true);
@@ -32,7 +46,7 @@ const Listing = () => {
                 setLoading(false);
             }
         }
-        fetchListing(params.listingId);
+        fetchListing();
     }, [params.listingId])
 
   return (
@@ -44,7 +58,7 @@ const Listing = () => {
             <p className="text-center my-7 text-2xl">Something went wrong!</p>
         )}
         {listing && !loading && !error && (
-            <>
+            <div>
                 <Swiper navigation>
                     {listing.imageUrls.map((url) => (
                         <SwiperSlide key={url}>
@@ -54,7 +68,73 @@ const Listing = () => {
                         </SwiperSlide>
                     ))}
                 </Swiper>
-            </>
+                <div className="fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer">
+                    <FaShare 
+                        className="text-slate-500"
+                        onClick={() => {
+                            navigator.clipboard.writeText(window.location.href);
+                            setCopied(true);
+                            setTimeout(() => {
+                                setCopied(false);
+                            }, 2000);
+                        }}
+                    />
+                </div>
+                {copied && (
+                    <p className="fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2">
+                        Link Copied!
+                    </p>
+                )}
+                <div className="flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4">
+                    <p className="text-2xl font-semibold">
+                        {listing.name} - ${' '}
+                        {listing.offer
+                            ? listing.discountPrice.toLocaleString('en-US')
+                            : listing.regularPrice.toLocaleString('en-US')
+                        }
+                        {listing.type === 'rent' && '/month'}
+                    </p>
+                    <p className="flex items-center mt-6 gap-2 text-slate-600 text-sm">
+                        <FaMapMarkedAlt className="text-green-700" />
+                        {listing.address}
+                    </p>
+                    <div className="flex gap-4">
+                        <p className="bg-blue-700 w-full max-w-[200px] text-white text-center p-1 rounded-md">
+                            {listing.type === 'rent' ? 'For Rent' : 'For Sale'}
+                        </p>
+                        { listing.offer && (
+                            <p className='bg-green-700 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
+                                ${+listing.regularPrice - +listing.discountPrice} OFF 
+                            </p>
+                        )}
+                    </div>
+                    <p className="text-slate-800">
+                        <span className="font-semibold text-black">Description - {' '}</span>
+                        {listing.description}
+                    </p>
+                    <ul className="text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6">
+                        <li className="flex items-center gap-1 whitespace-nowrap">
+                            <FaBed className="text-lg "/>
+                            {listing.bedrooms}
+                            {listing.bedrooms > 1 ? ' beds' : ' bed'}
+                        </li>
+                        <li className="flex items-center gap-1 whitespace-nowrap">
+                            <FaBath className="text-lg "/>
+                            {listing.bathrooms}
+                            {listing.bathrooms > 1 ? ' baths' : ' bath'}
+                        </li>
+                        <li className="flex items-center gap-1 whitespace-nowrap">
+                            <FaParking className="text-lg "/>
+                            {listing.parking ? 'Parking Spot' : 'No Parking'}
+                        </li>
+                        <li className="flex items-center gap-1 whitespace-nowrap">
+                            <FaChair className="text-lg "/>
+                            {listing.furnished ? 'Furnished' : ' Unfurnished'}
+                        </li>
+                        
+                    </ul>
+                </div>
+            </div>
         )}
     </main>
   )
