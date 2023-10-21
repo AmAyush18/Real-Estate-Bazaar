d99a72fce2d161db61eb7e2124651be5a482e655
Ayush Dwivedi <amayush0818@gmail.com>
2023-10-21 22:17:04 +0530

    Implemented Header Search Bar Functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,29 @@
 import { FaSearch } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
 
 const Header = () => {
-    const { currentUser } = useSelector(state => state.user)
+    const { currentUser } = useSelector(state => state.user);
+    const [searchTerm, setSearchTerm] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const urlParams = new URLSearchParams(window.location.search);
+        urlParams.set('searchTerm', searchTerm);
+        const searchQuery = urlParams.toString();
+        console.log(searchTerm, ' ', searchQuery);
+        navigate(`/search?${searchQuery}`);
+    }
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const searchTermFromUrl = urlParams.get('searchTerm'); 
+        if(searchTermFromUrl) {
+            setSearchTerm(searchTermFromUrl);
+        }
+    },[location.search])
 
   return (
     <header className='bg-slate-200 shadow-md'>
@@ -14,16 +34,18 @@ const Header = () => {
                 <span className='text-slate-700'>Bazaar</span>
             </h1>
         </Link>
-        <Link to='/search'>
-            <form action="" className='bg-slate-100 p-3 rounded-lg flex items-center'>
-                <input 
-                    type="text" 
-                    placeholder='Search..' 
-                    className='bg-transparent focus: outline-none w-24 sm:w-64' 
-                />
+        <form onSubmit={handleSubmit} className='bg-slate-100 p-3 rounded-lg flex items-center'>
+            <input 
+                onChange={(e) => setSearchTerm(e.target.value)}
+                type="text" 
+                placeholder='Search..' 
+                value={searchTerm}
+                className='bg-transparent focus: outline-none w-24 sm:w-64' 
+            />
+            <button>
                 <FaSearch className='text-slate-600' />
-            </form>
-        </Link>
+            </button>
+        </form>
         <ul className='flex gap-4'>
             <Link to='/'>
                 <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
@@ -44,4 +66,4 @@ const Header = () => {
   )
 }
 
-export default Header
+export default Header;
