7272e029e153f24babfee5a4d2c7046158340503
Ayush Dwivedi <amayush0818@gmail.com>
2023-10-22 17:34:45 +0530

    implemented pre installation for deployment

--- api/index.js
+++ api/index.js
@@ -8,6 +8,8 @@ import listingRouter from './routes/listing.route.js';
 
 import cookieParser from 'cookie-parser';
 
+import path from 'path';
+
 dotenv.config();
 
 mongoose
@@ -19,6 +21,7 @@ mongoose
         console.log(err);
     });
 
+const __dirname = path.resolve();
 const app = express();
 
 app.use(express.json());
@@ -32,6 +35,12 @@ app.listen(3000, () => {
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
